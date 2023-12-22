721c8ba76e510efd17bfda1971ea31e45cdce1bb
LahiruHarshana <lharshana2002@gmail.com>
2023-12-22 14:43:04 +0530

    updated

--- src/main/java/com/example/mappingspecification/MyServlet5.java
+++ src/main/java/com/example/mappingspecification/MyServlet5.java
@@ -12,5 +12,6 @@ import java.io.IOException;
 public class MyServlet5 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1 style='background-color: aquamarine'>Hello I am Servlet5</h1>");
     }
 }

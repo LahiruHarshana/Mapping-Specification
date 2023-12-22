98ee6cf8efe981e634f3849f770d4dbbb5141dbf
LahiruHarshana <lharshana2002@gmail.com>
2023-12-22 14:42:40 +0530

    updated

--- src/main/java/com/example/mappingspecification/MyServlet5.java
+++ src/main/java/com/example/mappingspecification/MyServlet5.java
@@ -1,7 +1,8 @@
 package com.example.mappingspecification;
 
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 
 @WebServlet(name = "MyServlet5", value = "/servlet5")
-public class MyServlet5 {
+public class MyServlet5 extends HttpServlet {
 }

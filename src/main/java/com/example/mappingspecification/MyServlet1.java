467a8dd9993ec15e29d1daaf85a4b921bfbe7408
LahiruHarshana <lharshana2002@gmail.com>
2023-12-22 14:00:24 +0530

    updated

--- src/main/java/com/example/mappingspecification/MyServlet1.java
+++ src/main/java/com/example/mappingspecification/MyServlet1.java
@@ -9,14 +9,7 @@ public class MyServlet1 extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter out=response.getWriter();
-        out.println("<h1>I am </h1>" +
-                "<h1>MyServlet1</h1>" +
-                "<h1>in the package</h1>" +
-                "<h1>com.example.mappingspecification</h1>" +
-                "<h1>with the url-pattern</h1>" +
-                "<h1>/servlet1</h1>" +
-                "<h1>in the web.xml file</h1>" +
-                "<h1>and the annotation</h1>");
+        out.println("<h1 style='background-color: aquamarine'>Hello</h1>");
     }
 
 }

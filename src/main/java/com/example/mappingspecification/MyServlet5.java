7808e29ce3909a031aaacd1b690b7fe13217f09b
LahiruHarshana <lharshana2002@gmail.com>
2023-12-22 14:42:58 +0530

    updated

--- src/main/java/com/example/mappingspecification/MyServlet5.java
+++ src/main/java/com/example/mappingspecification/MyServlet5.java
@@ -12,6 +12,5 @@ import java.io.IOException;
 public class MyServlet5 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
     }
 }

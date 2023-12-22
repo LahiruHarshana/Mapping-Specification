5cdc7072bd5301c3c48383c2956769c7923a84b1
LahiruHarshana <lharshana2002@gmail.com>
2023-12-22 13:46:34 +0530

    updated

--- src/main/java/com/example/mappingspecification/MyServlet1.java
+++ src/main/java/com/example/mappingspecification/MyServlet1.java
@@ -1,23 +1,22 @@
 package com.example.mappingspecification;
 
 import java.io.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "MyServlet1", value = "/servlet1")
 public class MyServlet1 extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        PrintWriter out=response.getWriter();
+        out.println("<h1>I am </h1>" +
+                "<h1>MyServlet1</h1>" +
+                "<h1>in the package</h1>" +
+                "<h1>com.example.mappingspecification</h1>" +
+                "<h1>with the url-pattern</h1>" +
+                "<h1>/servlet1</h1>" +
+                "<h1>in the web.xml file</h1>" +
+                "<h1>and the annotation</h1>");
     }
 
 }

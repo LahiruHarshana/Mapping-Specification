5894896ed401da2f375de460f2f1d009f7f8682c
LahiruHarshana <lharshana2002@gmail.com>
2023-12-22 14:44:26 +0530

    updated

--- src/main/java/com/example/mappingspecification/MyServlet5.java
+++ src/main/java/com/example/mappingspecification/MyServlet5.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name = "MyServlet5", value = "/servlet5")
+@WebServlet(name = "MyServlet5", value = "/")
 public class MyServlet5 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {

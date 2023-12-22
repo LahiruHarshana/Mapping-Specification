f37b7cb48ba12c81d3db77207227b1487fe07192
LahiruHarshana <lharshana2002@gmail.com>
2023-12-22 14:42:51 +0530

    updated

--- src/main/java/com/example/mappingspecification/MyServlet5.java
+++ src/main/java/com/example/mappingspecification/MyServlet5.java
@@ -1,8 +1,17 @@
 package com.example.mappingspecification;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet(name = "MyServlet5", value = "/servlet5")
 public class MyServlet5 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }

93959c22431bfbdf8d1ab82bbc074e3173c3a640
LahiruHarshana <lharshana2002@gmail.com>
2024-01-05 09:22:29 +0530

    updated

--- src/main/java/com/example/mappingspecification/MyServlet1.java
+++ src/main/java/com/example/mappingspecification/MyServlet1.java
@@ -4,11 +4,13 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "MyServlet1", value = "/hello")
+@WebServlet(name = "MyServlet1", value = "/hello/")
 public class MyServlet1 extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter out=response.getWriter();
         out.println("<h1 style='background-color: aquamarine'>Hello</h1>");
+
+        System.out.println(request.getContextPath());
     }
 }

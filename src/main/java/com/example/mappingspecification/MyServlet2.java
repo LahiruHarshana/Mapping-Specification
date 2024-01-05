bea0a7c605c0397c7c05a3b0ce68ced47994af9e
LahiruHarshana <lharshana2002@gmail.com>
2024-01-05 10:10:54 +0530

    updated

--- src/main/java/com/example/mappingspecification/MyServlet2.java
+++ src/main/java/com/example/mappingspecification/MyServlet2.java
@@ -9,11 +9,11 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name = "MyServlet2", value = "/hello/")
+@WebServlet(name = "MyServlet2", value = "/*")
 public class MyServlet2 extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.getWriter().println("<h1 style='background-color: aquamarine'>Hello I am Servlet2</h1>");
+
     }
 }

01d7d064bb40a838cd8b027ebf0596000a904634
LahiruHarshana <lharshana2002@gmail.com>
2024-01-05 09:57:44 +0530

    updated

--- src/main/java/com/example/mappingspecification/MyServlet4.java
+++ src/main/java/com/example/mappingspecification/MyServlet4.java
@@ -13,5 +13,12 @@ public class MyServlet4 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 resp.getWriter().println("<h1 style='background-color: aquamarine'>Hello I am Servlet4</h1>");
+
+        System.out.println(req.getContextPath());
+        System.out.println(req.getServletPath());
+        System.out.println(req.getPathInfo());
+        System.out.println(req.getRequestURI());
+        System.out.println(req.getQueryString());
+        System.out.println(req.getRequestURL());
     }
 }
